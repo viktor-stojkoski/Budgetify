f5c905b1aa1b939eaa62163deacdad9cc2e26f05
Viktor Stojkoski <36668908+viktor-stojkoski@users.noreply.github.com>
2021-07-28 13:30:49 +0200

    Created result class and string extensions with tests (#3)

---
+++ Api/Budgetify.Common/Contracts/Results/ResultType.cs
@@ -0,0 +1,13 @@
+﻿namespace Budgetify.Common.Contracts.Results
+{
+    public enum ResultType
+    {
+        InternalError = 0,
+        Ok = 1,
+        NotFound = 2,
+        Forbidden = 3,
+        Conflicted = 4,
+        Invalid = 5,
+        Unauthorized = 6
+    }
+}
---
+++ Tests/Api/Budgetify.Common.Tests/Extensions/StringExtensions/IsEmpty/IsEmptyShould.cs
@@ -0,0 +1,52 @@
+﻿namespace Budgetify.Common.Tests.Extensions.StringExtensions
+{
+    using Budgetify.Common.Extensions;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture(Category = nameof(IsEmptyShould))]
+    public class IsEmptyShould
+    {
+        [Test]
+        public void WhenNonEmptyString_ShouldReturnFalse()
+        {
+            // Arrange
+            string str = "Test string";
+
+            // Act
+            bool result = str.IsEmpty();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("    ")]
+        public void WhenWhitespaceAndEmptyString_ShouldReturnTrue(string str)
+        {
+            // Arrange
+
+            // Act
+            bool result = str.IsEmpty();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void WhenStringNull_ShouldReturnTrue()
+        {
+            // Arrange
+            string str = null;
+
+            // Act
+            bool result = str.IsEmpty();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
