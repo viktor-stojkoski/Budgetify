95d7a09145e721419301954a7b952ad9b6dcf75c
Viktor Stojkoski <36668908+viktor-stojkoski@users.noreply.github.com>
2022-01-04 16:56:53 +0100

    Created Azure Functions project (#20)

---
+++ Api/Budgetify.Functions/Startup.cs
@@ -0,0 +1,33 @@
+﻿[assembly: Microsoft.Azure.Functions.Extensions.DependencyInjection.FunctionsStartup(typeof(Budgetify.Functions.Startup))]
+namespace Budgetify.Functions;
+
+using System.IO;
+
+using Budgetify.Functions.Registers;
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        //FunctionsHostBuilderContext context = builder.GetContext();
+
+        builder.Services
+            .RegisterCommands();
+    }
+
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        base.ConfigureAppConfiguration(builder);
+
+        FunctionsHostBuilderContext context = builder.GetContext();
+
+        builder.ConfigurationBuilder
+            .AddJsonFile(
+                path: Path.Combine(context.ApplicationRootPath, "appsettings.json"),
+                optional: false,
+                reloadOnChange: true);
+    }
+}
