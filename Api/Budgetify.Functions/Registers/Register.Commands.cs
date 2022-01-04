1690f7d17391f64021bdd0c6863ab25a30de5793
Viktor Stojkoski <viktor.98@hotmail.com>
2022-01-04 15:56:31 +0100

    Created Azure Functions project

---
+++ Api/Budgetify.Functions/Registers/Register.Commands.cs
@@ -0,0 +1,21 @@
+﻿namespace Budgetify.Functions.Registers;
+
+using Budgetify.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using VS.Commands;
+
+public static partial class Register
+{
+    public static IServiceCollection RegisterCommands(this IServiceCollection services)
+    {
+        return services.AddCommands(new CommandOptions
+        {
+            Assemblies = new[]
+            {
+                typeof(CommandsAssemblyMarker).Assembly
+            }
+        });
+    }
+}
