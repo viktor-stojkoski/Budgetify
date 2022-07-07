5b3ae1151bb7e573120aa3bb6439af7c19420a89
Viktor Stojkoski <viktor.98@hotmail.com>
2022-07-07 17:37:48 +0200

    created sign in sign up user flow module

---
+++ Deployments/Infrastructure/Scripts/AzureAdB2C/Modules/NewSignInSignUpUserFlow.psm1
@@ -0,0 +1,73 @@
+Import-Module $PSScriptRoot\NewB2CIdentitiyUserFlow.psm1 -Force
+Import-Module $PSScriptRoot\NewB2CUserFlowAttributeAssignment.psm1 -Force
+Import-Module $PSScriptRoot\SetB2CUserFlowApiConnector.psm1 -Force
+
+function New-SignInSignUpUserFlow {
+  <#
+    .DESCRIPTION
+    Creates new Azure AD B2C SignUp and SignIn user flow.
+
+    .LINK
+    https://docs.microsoft.com/en-us/graph/api/b2cidentityuserflow-put-apiconnectorconfiguration?view=graph-rest-beta&tabs=http
+
+    .PARAMETER AccessToken
+    Access token for the Microsoft Graph API.
+
+    .PARAMETER ApiConnectorId
+    ID of the API Connector to update the B2C User Flow with.
+
+    .OUTPUTS
+    No response.
+
+    .EXAMPLE
+    Set-B2CUserFlowApiConnector `
+      -AccessToken @{ token_type, access_token } `
+      -UserFlowId "B2C_1_SignUpIn" `
+      -ApiConnectorId <ID_HERE>
+  #>
+  param (
+    [Parameter(Mandatory = $true)]
+    [psobject] $AccessToken,
+
+    [Parameter(Mandatory = $true)]
+    [string] $ApiConnectorId
+  )
+
+  ## Create SignUpIn user flow
+  $signUpSignInFlow = New-B2CIdentitiyUserFlow `
+    -AccessToken $AccessToken `
+    -Name "SignUpIn" `
+    -Type "signUpOrSignIn"
+
+  ## Create SignUpSignIn flow user attribute assignments
+  # City
+  New-B2CUserFlowAttributeAssignment `
+    -AccessToken $AccessToken `
+    -UserFlowId $signUpSignInFlow.id `
+    -UserInputType "textBox" `
+    -UserAttribute @{ id = "city" }
+
+  # Given name
+  New-B2CUserFlowAttributeAssignment `
+    -AccessToken $AccessToken `
+    -UserFlowId $signUpSignInFlow.id `
+    -UserInputType "textBox" `
+    -UserAttribute @{ id = "givenName" }
+
+  # Surname
+  New-B2CUserFlowAttributeAssignment `
+    -AccessToken $AccessToken `
+    -UserFlowId $signUpSignInFlow.id `
+    -UserInputType "textBox" `
+    -UserAttribute @{ id = "surname" }
+
+  ## Application Claims [NOT YET SUPPORTED]
+
+  ## API Connector
+  Set-B2CUserFlowApiConnector `
+    -AccessToken $AccessToken `
+    -UserFlowId $signUpSignInFlow.id `
+    -ApiConnectorId $ApiConnectorId
+}
+
+Export-ModuleMember -Function New-SignInSignUpUserFlow
