30464d8ce0060618cc80e829afdd5ac55117640d
Viktor Stojkoski <viktor.98@hotmail.com>
2022-08-03 23:56:04 +0200

    testing sensitive

--- Deployments/Infrastructure/Terraform/Test/main.tf
+++ Deployments/Infrastructure/Terraform/Test/main.tf
@@ -36,5 +36,7 @@ output "resource_group_name" {
 }
 
 output "storage_account_name" {
-  value = module.storage_account.storage_account_name
+  value       = module.storage_account.storage_account_name
+  sensitive   = true
+  description = "Test"
 }
