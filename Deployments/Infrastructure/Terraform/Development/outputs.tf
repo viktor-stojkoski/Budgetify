4f7dfbed4ab73ddcb93a40e08dde35e935378752
Viktor Stojkoski <viktor.98@hotmail.com>
2022-08-08 17:15:35 +0200

    fix

--- Deployments/Infrastructure/Terraform/Development/outputs.tf
+++ Deployments/Infrastructure/Terraform/Development/outputs.tf
@@ -15,7 +15,7 @@ output "b2c_tenant_id" {
 }
 
 output "tenant_id" {
-  value       = data.azuread_client_config.current.tenant_id
+  value       = data.azurerm_client_config.current.tenant_id
   description = "Current tenant ID"
 }
 
