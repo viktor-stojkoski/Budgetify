30d6885dd84359a8b7912a67917071a83227d61a
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-03 15:31:38 +0200

    updated queries project with currency entity

---
+++ Api/Budgetify.Queries/Infrastructure/Configuration/CurrencyConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Budgetify.Queries.Infrastructure.Configuration;
+
+using Budgetify.Queries.Common.Configuration;
+using Budgetify.Queries.Currency.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal class CurrencyConfiguration : EntityTypeConfigurationBase<Currency>, IEntityTypeConfiguration<Currency>
+{
+    public void Configure(EntityTypeBuilder<Currency> builder)
+    {
+        builder.ToTable("Currency", "Budgetify");
+
+        ConfigureDefaultColumns(builder);
+
+        builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("nvarchar(255)").IsRequired();
+        builder.Property(x => x.Code).HasColumnName("Code").HasColumnType("nvarchar(3)").IsRequired();
+        builder.Property(x => x.Symbol).HasColumnName("Symbol").HasColumnType("nvarchar(50)");
+    }
+}
