bdaa3e87b9d6d57bd75a11f3d99400dc2e229ff7
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-03 15:26:30 +0200

    updated storage project with currency entity

---
+++ Api/Budgetify.Storage/Currency/Currency.cs
@@ -0,0 +1,28 @@
+﻿namespace Budgetify.Storage.Currency;
+
+using System;
+
+using Budgetify.Storage.Common.Entities;
+
+public class Currency : AggregateRoot
+{
+    public Currency(
+        int id,
+        Guid uid,
+        DateTime createdOn,
+        DateTime? deletedOn,
+        string name,
+        string code,
+        string symbol) : base(id, uid, createdOn, deletedOn)
+    {
+        Name = name;
+        Code = code;
+        Symbol = symbol;
+    }
+
+    public string Name { get; protected internal set; }
+
+    public string Code { get; protected internal set; }
+
+    public string Symbol { get; protected internal set; }
+}
