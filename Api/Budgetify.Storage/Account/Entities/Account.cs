6fd98ac0c785a510deb5390e471a69faddd03635
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-05 00:00:32 +0200

    added entities in storage project

---
+++ Api/Budgetify.Storage/Account/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿namespace Budgetify.Storage.Account.Entities;
+
+using System;
+
+using Budgetify.Storage.Common.Entities;
+using Budgetify.Storage.Currency.Entities;
+using Budgetify.Storage.User.Entities;
+
+public class Account : AggregateRoot
+{
+    public Account(
+        int id,
+        Guid uid,
+        DateTime createdOn,
+        DateTime? deletedOn,
+        int userFk,
+        string name,
+        string type,
+        decimal balance,
+        int currencyFk,
+        string? description,
+        User user,
+        Currency currency) : base(id, uid, createdOn, deletedOn)
+    {
+        UserFk = userFk;
+        Name = name;
+        Type = type;
+        Balance = balance;
+        CurrencyFk = currencyFk;
+        Description = description;
+
+        User = user;
+        Currency = currency;
+    }
+
+    public int UserFk { get; protected internal set; }
+
+    public string Name { get; protected internal set; }
+
+    public string Type { get; protected internal set; }
+
+    public decimal Balance { get; protected internal set; }
+
+    public int CurrencyFk { get; protected internal set; }
+
+    public string? Description { get; protected internal set; }
+
+    public virtual User User { get; protected internal set; }
+
+    public virtual Currency Currency { get; protected internal set; }
+}
---
+++ Api/Budgetify.Storage/Infrastructure/Configuration/AccountConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace Budgetify.Storage.Infrastructure.Configuration;
+
+using Budgetify.Storage.Account.Entities;
+using Budgetify.Storage.Common.Configuration;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal class AccountConfiguration : EntityTypeConfigurationBase<Account>, IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.ToTable("Account", "Budgetify");
+
+        ConfigureDefaultColumns(builder);
+
+        builder.Property(x => x.UserFk).HasColumnName("UserFk").HasColumnType("int").IsRequired();
+        builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("nvarchar(255)").IsRequired();
+        builder.Property(x => x.Type).HasColumnName("Type").HasColumnType("nvarchar(50)").IsRequired();
+        builder.Property(x => x.Balance).HasColumnName("Balance").HasColumnType("decimal(19,4)").IsRequired();
+        builder.Property(x => x.CurrencyFk).HasColumnName("CurrencyFk").HasColumnType("int").IsRequired();
+        builder.Property(x => x.Type).HasColumnName("Type").HasColumnType("nvarchar(MAX)");
+
+        builder.HasOne(x => x.User).WithMany(x => x.Accounts).HasForeignKey(x => x.UserFk);
+        builder.HasOne(x => x.Currency).WithMany(x => x.Accounts).HasForeignKey(x => x.CurrencyFk);
+
+        builder.Ignore(x => x.DomainEvents);
+    }
+}
