c91c4b46c99f3a898fc5f0992f8228561eac9b48
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-15 15:45:14 +0200

    fix tests and spell check

--- Clients/Angular/src/app/components/navbar/navbar.component.spec.ts
+++ Clients/Angular/src/app/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppModule } from '../../app.module';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -8,9 +9,9 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+      imports: [AppModule],
+      declarations: [NavbarComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
