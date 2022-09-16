267d31f48e5cedaf45d4d02a4550a905b2a3b0f6
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-16 15:22:05 +0200

    added translations

---
+++ Clients/Angular/src/app/static/translationKeys.ts
@@ -0,0 +1,9 @@
+export class TranslationKeys {
+  public static readonly navbarAccountsMenu = 'navbarAccountsMenu';
+  public static readonly navbarTransactionsMenu = 'navbarTransactionsMenu';
+  public static readonly navbarLoginOrRegister = 'navbarLoginOrRegister';
+  public static readonly navbarLogout = 'navbarLogout';
+  public static readonly navbarLanguages = 'navbarLanguages';
+  public static readonly navbarLanguageEnglish = 'navbarLanguageEnglish';
+  public static readonly navbarLanguageMacedonian = 'navbarLanguageMacedonian';
+}
