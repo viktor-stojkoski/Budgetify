389d3d7a3c20e370508c80baaee986db3f89fc70
Viktor Stojkoski <viktor.98@hotmail.com>
2022-09-29 16:42:21 +0200

    create get currencies query

---
+++ Api/Budgetify.Api/Controllers/CurrenciesController.cs
@@ -0,0 +1,27 @@
+﻿namespace Budgetify.Api.Controllers;
+
+using System.Threading.Tasks;
+
+using Budgetify.Queries.Currency.Queries.GetCurrencies;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using VS.Queries;
+
+[Route("api/currencies")]
+[Authorize]
+[ApiController]
+public class CurrenciesController : ExtendedApiController
+{
+    private readonly IQueryDispatcher _queryDispatcher;
+
+    public CurrenciesController(IQueryDispatcher queryDispatcher)
+    {
+        _queryDispatcher = queryDispatcher;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCurrenciesAsync() =>
+        OkOrError(await _queryDispatcher.ExecuteAsync(new GetCurrenciesQuery()));
+}
