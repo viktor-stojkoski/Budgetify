7ef4582154dd8ee04b2e48168acc51f25c6d5670
Viktor Stojkoski <viktor.98@hotmail.com>
2022-10-02 18:19:25 +0200

    fixed translateEnum pipe

--- Clients/Angular/src/app/shared/pipes/translate-enum.pipe.ts
+++ Clients/Angular/src/app/shared/pipes/translate-enum.pipe.ts
@@ -2,12 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
-  name: 'translateEnum'
+  name: 'translateEnum',
+  pure: false
 })
 export class TranslateEnumPipe implements PipeTransform {
+  private translatedValue: string = '';
+
   constructor(private translateService: TranslateService) {}
 
   public transform(value: string, enumType: { [x: string]: string }): string {
-    return this.translateService.instant(enumType[value]);
+    if (this.translateService.instant(enumType[value]) === this.translatedValue) {
+      return this.translatedValue;
+    }
+
+    this.translatedValue = this.translateService.instant(enumType[value]);
+
+    return this.translatedValue;
   }
 }
