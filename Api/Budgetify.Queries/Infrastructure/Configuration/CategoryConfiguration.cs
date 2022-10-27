4fefc471d079260383957fce1a99c6caba7fa2c1
Viktor Stojkoski <viktor.98@hotmail.com>
2022-10-27 16:18:49 +0200

    created entities and configuration in queries

---
+++ Api/Budgetify.Queries/Infrastructure/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Budgetify.Queries.Infrastructure.Configuration;
+
+using Budgetify.Queries.Common.Configuration;
+using Budgetify.Queries.Currency.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal class CategoryConfiguration : EntityTypeConfigurationBase<Category>, IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Category", "Budgetify");
+
+        ConfigureDefaultColumns(builder);
+
+        builder.Property(x => x.UserId).HasColumnName("UserFk").HasColumnType("int").IsRequired();
+        builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("nvarchar(255)").IsRequired();
+        builder.Property(x => x.Type).HasColumnName("Type").HasColumnType("nvarchar(50)").IsRequired().HasDefaultValue("");
+
+        builder.HasOne(x => x.User).WithMany(x => x.Categories).HasForeignKey(x => x.UserId);
+    }
+}
