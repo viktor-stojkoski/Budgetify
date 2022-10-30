a05e7453838e555c05703646708edd66494315dd
Viktor Stojkoski <viktor.98@hotmail.com>
2022-10-30 16:07:46 +0100

    created query for getting categories

---
+++ Api/Budgetify.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿namespace Budgetify.Api.Controllers;
+
+using System.Threading.Tasks;
+
+using Budgetify.Queries.Category.Queries.GetCategories;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using VS.Queries;
+
+[Route("api/categories")]
+[Authorize]
+[ApiController]
+public class CategoriesController : ExtendedApiController
+{
+    private readonly IQueryDispatcher _queryDispatcher;
+
+    public CategoriesController(IQueryDispatcher queryDispatcher)
+    {
+        _queryDispatcher = queryDispatcher;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategoriesAsync() =>
+        OkOrError(await _queryDispatcher.ExecuteAsync(new GetCategoriesQuery()));
+}
---
+++ Api/Budgetify.Queries/Category/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,47 @@
+﻿namespace Budgetify.Queries.Category.Queries.GetCategories;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Budgetify.Common.CurrentUser;
+using Budgetify.Queries.Currency.Entities;
+using Budgetify.Queries.Infrastructure.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+using VS.Queries;
+
+public record GetCategoriesQuery() : IQuery<IEnumerable<CategoryResponse>>;
+
+public class GetCategoriesQueryHandler : IQueryHandler<GetCategoriesQuery, IEnumerable<CategoryResponse>>
+{
+    private readonly IBudgetifyReadonlyDbContext _budgetifyReadonlyDbContext;
+    private readonly ICurrentUser _currentUser;
+
+    public GetCategoriesQueryHandler(
+        IBudgetifyReadonlyDbContext budgetifyReadonlyDbContext,
+        ICurrentUser currentUser)
+    {
+        _budgetifyReadonlyDbContext = budgetifyReadonlyDbContext;
+        _currentUser = currentUser;
+    }
+
+    public async Task<QueryResult<IEnumerable<CategoryResponse>>> ExecuteAsync(GetCategoriesQuery query)
+    {
+        QueryResultBuilder<IEnumerable<CategoryResponse>> result = new();
+
+        IEnumerable<CategoryResponse> categories =
+            await _budgetifyReadonlyDbContext.AllNoTrackedOf<Category>()
+                .Where(x => x.UserId == _currentUser.Id)
+                .Select(x => new CategoryResponse(
+                    x.Uid,
+                    x.Name,
+                    x.Type))
+                .ToListAsync();
+
+        result.SetValue(categories);
+
+        return result.Build();
+    }
+}
