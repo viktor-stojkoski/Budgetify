63eb467606c93f1c064db378cf61587aad6df019
Viktor Stojkoski <viktor.98@hotmail.com>
2022-11-02 00:08:43 +0100

    cleanup

--- Clients/Angular/src/app/shared/static/translationKeys.ts
+++ Clients/Angular/src/app/shared/static/translationKeys.ts
@@ -2,6 +2,7 @@ export class TranslationKeys {
   public static readonly navbarAccountsMenu = 'navbarAccountsMenu';
   public static readonly navbarTransactionsMenu = 'navbarTransactionsMenu';
   public static readonly navbarCategoriesMenu = 'navbarCategoriesMenu';
+  public static readonly navbarMerchantsMenu = 'navbarMerchantsMenu';
   public static readonly navbarLoginOrRegister = 'navbarLoginOrRegister';
   public static readonly navbarLogout = 'navbarLogout';
   public static readonly navbarLanguages = 'navbarLanguages';
