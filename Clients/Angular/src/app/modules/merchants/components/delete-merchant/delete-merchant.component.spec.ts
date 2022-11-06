ae6688e4f01a7d548b900345dd4aa89ffbd03a84
Viktor Stojkoski <viktor.98@hotmail.com>
2022-11-06 15:51:34 +0100

    fix tests

--- Clients/Angular/src/app/modules/merchants/components/delete-merchant/delete-merchant.component.spec.ts
+++ Clients/Angular/src/app/modules/merchants/components/delete-merchant/delete-merchant.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppModule } from '../../../../app.module';
+import { MerchantsModule } from '../../merchants.module';
 
 import { DeleteMerchantComponent } from './delete-merchant.component';
 
@@ -8,9 +11,10 @@ describe('DeleteMerchantComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteMerchantComponent ]
-    })
-    .compileComponents();
+      imports: [MerchantsModule, AppModule],
+      declarations: [DeleteMerchantComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteMerchantComponent);
     component = fixture.componentInstance;
--- Clients/Angular/src/app/shared/pipes/truncate.pipe.ts
+++ Clients/Angular/src/app/shared/pipes/truncate.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate'
 })
 export class TruncatePipe implements PipeTransform {
-  public transform(value: string, length: number = 50, suffix: string = '...'): string {
-    return value.length < length ? value : value.slice(0, length) + suffix;
+  public transform(value?: string, length: number = 50, suffix: string = '...'): string {
+    return value ? (value.length < length ? value : value.slice(0, length) + suffix) : '';
   }
 }
