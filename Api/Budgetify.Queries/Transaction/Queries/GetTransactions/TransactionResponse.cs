a4898182da4448f24caee6728633879aa0cb75dd
Viktor Stojkoski <viktor.98@hotmail.com>
2022-11-22 21:52:35 +0100

    created get transactions query and transactions controller

---
+++ Api/Budgetify.Queries/Transaction/Queries/GetTransactions/TransactionResponse.cs
@@ -0,0 +1,46 @@
+﻿namespace Budgetify.Queries.Transaction.Queries.GetTransactions;
+
+using System;
+
+public class TransactionResponse
+{
+    public TransactionResponse(
+        Guid uid,
+        string accountName,
+        string categoryName,
+        string currencyCode,
+        string? merchantName,
+        string type,
+        decimal amount,
+        DateTime date,
+        string? description)
+    {
+        Uid = uid;
+        AccountName = accountName;
+        CategoryName = categoryName;
+        CurrencyCode = currencyCode;
+        MerchantName = merchantName;
+        Type = type;
+        Amount = amount;
+        Date = date;
+        Description = description;
+    }
+
+    public Guid Uid { get; set; }
+
+    public string AccountName { get; set; }
+
+    public string CategoryName { get; set; }
+
+    public string CurrencyCode { get; set; }
+
+    public string? MerchantName { get; set; }
+
+    public string Type { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string? Description { get; set; }
+}
