7638228614b7886399a0ef37f0155228e23df2ed
Viktor Stojkoski <viktor.98@hotmail.com>
2022-12-14 15:16:02 +0100

    created endpoint and command for getting exchange rate details

---
+++ Api/Budgetify.Queries/ExchangeRate/Queries/GetExchangeRate/ExchangeRateResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace Budgetify.Queries.ExchangeRate.Queries.GetExchangeRate;
+
+using System;
+
+public class ExchangeRateResponse
+{
+    public ExchangeRateResponse(
+        string fromCurrencyCode,
+        string toCurrencyCode,
+        DateTime? fromDate,
+        DateTime? toDate,
+        decimal rate)
+    {
+        FromCurrencyCode = fromCurrencyCode;
+        ToCurrencyCode = toCurrencyCode;
+        FromDate = fromDate;
+        ToDate = toDate;
+        Rate = rate;
+    }
+
+    public string FromCurrencyCode { get; set; }
+
+    public string ToCurrencyCode { get; set; }
+
+    public DateTime? FromDate { get; set; }
+
+    public DateTime? ToDate { get; set; }
+
+    public decimal Rate { get; set; }
+}
