8680c6c1968908f00d09bf76ce33bf64277496af
Viktor Stojkoski <viktor.98@hotmail.com>
2023-01-28 15:59:16 +0100

    fix tests

--- Clients/Angular/src/app/modules/transactions/components/delete-transaction-attachment/delete-transaction-attachment.component.spec.ts
+++ Clients/Angular/src/app/modules/transactions/components/delete-transaction-attachment/delete-transaction-attachment.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppModule } from '../../../../app.module';
+import { TransactionsModule } from '../../transactions.module';
 
 import { DeleteTransactionAttachmentComponent } from './delete-transaction-attachment.component';
 
@@ -8,9 +11,10 @@ describe('DeleteTransactionAttachmentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteTransactionAttachmentComponent ]
-    })
-    .compileComponents();
+      imports: [TransactionsModule, AppModule],
+      declarations: [DeleteTransactionAttachmentComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteTransactionAttachmentComponent);
     component = fixture.componentInstance;
