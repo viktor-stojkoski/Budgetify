0a5c947e89b99a219ba3bbbfb13dc2b7dfc1755a
Viktor Stojkoski <viktor.98@hotmail.com>
2023-01-29 17:55:38 +0100

    fix tests

--- Clients/Angular/src/app/modules/transactions/components/add-transaction-attachments/add-transaction-attachments.component.spec.ts
+++ Clients/Angular/src/app/modules/transactions/components/add-transaction-attachments/add-transaction-attachments.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppModule } from '../../../../app.module';
+import { TransactionsModule } from '../../transactions.module';
 
 import { AddTransactionAttachmentsComponent } from './add-transaction-attachments.component';
 
@@ -8,9 +11,10 @@ describe('AddTransactionAttachmentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddTransactionAttachmentsComponent ]
-    })
-    .compileComponents();
+      imports: [TransactionsModule, AppModule],
+      declarations: [AddTransactionAttachmentsComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddTransactionAttachmentsComponent);
     component = fixture.componentInstance;
