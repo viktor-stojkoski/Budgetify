fbc469256e94df13607abbb86d3950d4f118aa25
Viktor Stojkoski <viktor.98@hotmail.com>
2023-02-14 23:01:44 +0100

    remove scanservice from command

--- Api/Budgetify.Services/Transaction/Commands/AddTransactionAttachmentsCommand.cs
+++ Api/Budgetify.Services/Transaction/Commands/AddTransactionAttachmentsCommand.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 using Budgetify.Common.CurrentUser;
 using Budgetify.Common.Results;
-using Budgetify.Common.ScanReceipt;
 using Budgetify.Common.Storage;
 using Budgetify.Contracts.Infrastructure.Storage;
 using Budgetify.Contracts.Settings;
@@ -28,22 +27,19 @@ public class AddTransactionAttachmentsCommandHandler : ICommandHandler<AddTransa
     private readonly IStorageService _storageService;
     private readonly IStorageSettings _storageSettings;
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IScanReceiptService _scanReceiptService;
 
     public AddTransactionAttachmentsCommandHandler(
         ICurrentUser currentUser,
         ITransactionRepository transactionRepository,
         IStorageService storageService,
         IStorageSettings storageSettings,
-        IUnitOfWork unitOfWork,
-        IScanReceiptService scanReceiptService)
+        IUnitOfWork unitOfWork)
     {
         _currentUser = currentUser;
         _transactionRepository = transactionRepository;
         _storageService = storageService;
         _storageSettings = storageSettings;
         _unitOfWork = unitOfWork;
-        _scanReceiptService = scanReceiptService;
     }
 
     public async Task<CommandResult<EmptyValue>> ExecuteAsync(AddTransactionAttachmentsCommand command)
@@ -84,12 +80,7 @@ public class AddTransactionAttachmentsCommandHandler : ICommandHandler<AddTransa
                         contentType: attachment.Type));
             }
 
-            UploadedFileResponse[] response = await Task.WhenAll(attachmentsForUploadTasks);
-
-            SignedUrlResponse test =
-                await _storageService.GetSignedUrlAsync(_storageSettings.ContainerName, response.First().FileName, DateTime.UtcNow.AddHours(3));
-
-            _ = await _scanReceiptService.ScanReceiptAsync(test.Url);
+            await Task.WhenAll(attachmentsForUploadTasks);
 
             _transactionRepository.Update(transactionResult.Value);
 
