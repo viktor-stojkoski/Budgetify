94952cc97d519aae260b16f1dd9cadea88104579
Viktor Stojkoski <viktor.98@hotmail.com>
2023-06-12 22:57:59 +0200

    created query for getting budgets

---
+++ Api/Budgetify.Api/Controllers/BudgetsController.cs
@@ -0,0 +1,27 @@
+﻿namespace Budgetify.Api.Controllers;
+
+using System.Threading.Tasks;
+
+using Budgetify.Queries.Budget.Queries.GetBudgets;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using VS.Queries;
+
+[Route("api/budgets")]
+[ApiController]
+[Authorize]
+public class BudgetsController : ExtendedApiController
+{
+    private readonly IQueryDispatcher _queryDispatcher;
+
+    public BudgetsController(IQueryDispatcher queryDispatcher)
+    {
+        _queryDispatcher = queryDispatcher;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetButgetsAsync() =>
+        OkOrError(await _queryDispatcher.ExecuteAsync(new GetBudgetsQuery()));
+}
