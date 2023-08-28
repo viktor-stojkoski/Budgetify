464f355f9b07261f05a3763bdc583f0024bc6f9b
Viktor Stojkoski <viktor.98@hotmail.com>
2023-08-28 15:06:41 +0200

    fix tests

--- Clients/Angular/src/app/modules/budgets/components/delete-budget/delete-budget.component.spec.ts
+++ Clients/Angular/src/app/modules/budgets/components/delete-budget/delete-budget.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AppModule } from '../../../../app.module';
 import { BudgetsModule } from '../../budgets.module';
 import { DeleteBudgetComponent } from './delete-budget.component';
@@ -11,7 +12,8 @@ describe('DeleteBudgetComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BudgetsModule, AppModule],
-      declarations: [DeleteBudgetComponent]
+      declarations: [DeleteBudgetComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteBudgetComponent);
