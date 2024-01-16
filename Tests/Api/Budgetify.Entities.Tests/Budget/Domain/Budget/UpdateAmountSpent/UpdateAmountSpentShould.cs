5edad02c105a0923af28cdd7e4b2410068e457d2
Viktor Stojkoski <viktor.98@hotmail.com>
2024-01-16 22:49:44 +0100

    added tests for UpdateAmountSpent

---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/Domain/Budget/UpdateAmountSpent/UpdateAmountSpentShould.cs
@@ -0,0 +1,30 @@
+﻿namespace Budgetify.Entities.Tests.Budget.Domain.Budget;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.Domain;
+using Budgetify.Entities.Common.Enumerations;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(UpdateAmountSpentShould))]
+public class UpdateAmountSpentShould
+{
+    [Test]
+    public void WhenAmountSpentUpdated_WillUpdateAmountSpent()
+    {
+        // Arrange
+        Budget budget = new BudgetBuilder()
+            .Build();
+
+        // Act
+        Result result = budget.UpdateAmountSpent(2000);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        budget.State.Should().Be(EntityState.Modified);
+        budget.AmountSpent.Should().Be(3000);
+    }
+}
