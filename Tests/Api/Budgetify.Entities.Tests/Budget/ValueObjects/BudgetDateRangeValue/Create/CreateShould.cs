40f0042ef5dc4ed30ee9e6a98f2c7b6fc5426b03
Viktor Stojkoski <viktor.98@hotmail.com>
2024-01-16 23:04:25 +0100

    added tests for BudgetDateRange value object

---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/ValueObjects/BudgetDateRangeValue/Create/CreateShould.cs
@@ -0,0 +1,48 @@
+﻿namespace Budgetify.Entities.Tests.Budget.ValueObjects.BudgetDateRangeValue;
+
+using System;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.ValueObjects;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(CreateShould))]
+public class CreateShould
+{
+    [Test]
+    public void WhenStartDateGreaterThanEndDate_WillReturnErrorResult()
+    {
+        // Arrange
+        DateTime startDate = new(2024, 2, 1);
+        DateTime endDate = new(2024, 1, 1);
+
+        // Act
+        Result<BudgetDateRangeValue> result =
+            BudgetDateRangeValue.Create(startDate, endDate);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.BudgetStartDateCannotBeGreaterThanEndDate);
+    }
+
+    [Test]
+    public void WhenArgumentsCorrect_WillCreateBudgetDateRange()
+    {
+        // Arrange
+        DateTime startDate = new(2024, 1, 1);
+        DateTime endDate = new(2024, 2, 1);
+
+        // Act
+        Result<BudgetDateRangeValue> result =
+            BudgetDateRangeValue.Create(startDate, endDate);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        result.Value.StartDate.Should().Be(startDate);
+        result.Value.EndDate.Should().Be(endDate);
+    }
+}
---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/ValueObjects/BudgetDateRangeValue/Equals/EqualsShould.cs
@@ -0,0 +1,54 @@
+﻿namespace Budgetify.Entities.Tests.Budget.ValueObjects.BudgetDateRangeValue;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.ValueObjects;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(EqualsShould))]
+public class EqualsShould
+{
+    [Test]
+    public void WhenNotEqual_WillReturnFalse()
+    {
+        // Arrange
+        Result<BudgetDateRangeValue> value1 =
+            BudgetDateRangeValue.Create(
+                startDate: new(2024, 1, 1),
+                endDate: new(2024, 2, 1));
+
+        Result<BudgetDateRangeValue> value2 =
+            BudgetDateRangeValue.Create(
+                startDate: new(2024, 1, 1),
+                endDate: new(2024, 3, 1));
+
+        // Act
+        bool result = value1.Value == value2.Value;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Test]
+    public void WhenEqual_WillReturnTrue()
+    {
+        // Arrange
+        Result<BudgetDateRangeValue> value1 =
+            BudgetDateRangeValue.Create(
+                startDate: new(2024, 1, 1),
+                endDate: new(2024, 2, 1));
+
+        Result<BudgetDateRangeValue> value2 =
+            BudgetDateRangeValue.Create(
+                startDate: new(2024, 1, 1),
+                endDate: new(2024, 2, 1));
+
+        // Act
+        bool result = value1.Value == value2.Value;
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
