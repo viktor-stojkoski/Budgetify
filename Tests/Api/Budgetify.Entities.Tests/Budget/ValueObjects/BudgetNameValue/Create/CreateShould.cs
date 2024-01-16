0895f61e858f08f00459a32db1c6a06c0f6310fe
Viktor Stojkoski <viktor.98@hotmail.com>
2024-01-16 23:14:35 +0100

    added tests for budgetname value object

---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/ValueObjects/BudgetNameValue/Create/CreateShould.cs
@@ -0,0 +1,72 @@
+﻿namespace Budgetify.Entities.Tests.Budget.ValueObjects.BudgetNameValue;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.ValueObjects;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using static CommonTestsHelper;
+
+[TestFixture(Category = nameof(CreateShould))]
+public class CreateShould
+{
+    [Test]
+    public void WhenValueNull_WillReturnErrorResult()
+    {
+        // Arrange
+        string? value = null;
+
+        // Act
+        Result<BudgetNameValue> result = BudgetNameValue.Create(value);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.BudgetNameInvalid);
+    }
+
+    [Test]
+    public void WhenValueEmptyString_WillReturnErrorResult()
+    {
+        // Arrange
+        string value = "";
+
+        // Act
+        Result<BudgetNameValue> result = BudgetNameValue.Create(value);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.BudgetNameInvalid);
+    }
+
+    [Test]
+    public void WhenValueHasMoreThan255Characters_WillReturnErrorResult()
+    {
+        // Arrange
+        string value = RandomString(256);
+
+        // Act
+        Result<BudgetNameValue> result = BudgetNameValue.Create(value);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.BudgetNameInvalidLength);
+    }
+
+    [Test]
+    public void WhenValueCorrect_WillCreateBudgetName()
+    {
+        // Arrange
+        string value = "Test Budget Name";
+
+        // Act
+        Result<BudgetNameValue> result = BudgetNameValue.Create(value);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        string implicitOperatorResult = result.Value;
+        implicitOperatorResult.Should().Be(value);
+    }
+}
---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/ValueObjects/BudgetNameValue/Equals/EqualsShould.cs
@@ -0,0 +1,40 @@
+﻿namespace Budgetify.Entities.Tests.Budget.ValueObjects.BudgetNameValue;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.ValueObjects;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(EqualsShould))]
+public class EqualsShould
+{
+    [Test]
+    public void WhenNotEqual_WillReturnFalse()
+    {
+        // Arrange
+        Result<BudgetNameValue> value1 = BudgetNameValue.Create("Test Budget Name 1");
+        Result<BudgetNameValue> value2 = BudgetNameValue.Create("Test Budget Name 2");
+
+        // Act
+        bool result = value1.Value == value2.Value;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Test]
+    public void WhenEqual_WillReturnTrue()
+    {
+        // Arrange
+        Result<BudgetNameValue> value1 = BudgetNameValue.Create("Test Budget Name");
+        Result<BudgetNameValue> value2 = BudgetNameValue.Create("Test Budget Name");
+
+        // Act
+        bool result = value1.Value == value2.Value;
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
