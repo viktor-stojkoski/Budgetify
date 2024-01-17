52324432277188bba607a1299f282bc481d0c32a
Viktor Stojkoski <36668908+viktor-stojkoski@users.noreply.github.com>
2024-01-17 11:17:23 +0100

    Budget domain tests (#99)

---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/Domain/Budget/BudgetBuilder.cs
@@ -0,0 +1,44 @@
+﻿namespace Budgetify.Entities.Tests.Budget.Domain.Budget;
+
+using System;
+
+using Budgetify.Entities.Budget.Domain;
+
+internal class BudgetBuilder
+{
+    private readonly int _id = 1;
+    private readonly Guid _uid = Guid.NewGuid();
+    private readonly DateTime _createdOn = new(2024, 1, 16);
+    private DateTime? _deletedOn = null;
+    private readonly int _userId = 2;
+    private readonly string _name = "Name";
+    private readonly int _categoryId = 3;
+    private readonly int _currencyId = 4;
+    private readonly DateTime _startDate = new(2024, 1, 1);
+    private readonly DateTime _endDate = new(2024, 2, 1);
+    private readonly decimal _amount = 10000;
+    private readonly decimal _amountSpent = 1000;
+
+    internal Budget Build()
+    {
+        return Budget.Create(
+            id: _id,
+            uid: _uid,
+            createdOn: _createdOn,
+            deletedOn: _deletedOn,
+            userId: _userId,
+            name: _name,
+            categoryId: _categoryId,
+            currencyId: _currencyId,
+            startDate: _startDate,
+            endDate: _endDate,
+            amount: _amount,
+            amountSpent: _amountSpent).Value;
+    }
+
+    internal BudgetBuilder WithDeletedOn(DateTime deletedOn)
+    {
+        _deletedOn = deletedOn;
+        return this;
+    }
+}
---
+++ Tests/Api/Budgetify.Entities.Tests/Budget/Domain/Budget/Update/UpdateShould.cs
@@ -0,0 +1,56 @@
+﻿namespace Budgetify.Entities.Tests.Budget.Domain.Budget;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Budget.Domain;
+using Budgetify.Entities.Common.Enumerations;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(UpdateShould))]
+public class UpdateShould
+{
+    [Test]
+    public void WhenNameInvalid_WillReturnErrorResult()
+    {
+        // Arrange
+        string name = "";
+        decimal amount = 1000;
+
+        Budget budget = new BudgetBuilder()
+            .Build();
+
+        // Act
+        Result result = budget.Update(name, amount);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.BudgetNameInvalid);
+
+        budget.State.Should().Be(EntityState.Unchanged);
+        budget.Name.Value.Should().Be("Name");
+        budget.Amount.Should().Be(10000);
+    }
+
+    [Test]
+    public void WhenArgumentsCorrect_WillUpdateBudget()
+    {
+        // Arrange
+        string name = "New Name";
+        decimal amount = 1000;
+
+        Budget budget = new BudgetBuilder()
+            .Build();
+
+        // Act
+        Result result = budget.Update(name, amount);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        budget.State.Should().Be(EntityState.Modified);
+        budget.Name.Value.Should().Be(name);
+        budget.Amount.Should().Be(1000);
+    }
+}
