7d08adc5f883ff513662e5303ba534e97435fe9b
Viktor Stojkoski <viktor.98@hotmail.com>
2024-01-17 15:22:35 +0100

    delete tests

---
+++ Tests/Api/Budgetify.Entities.Tests/Category/Domain/Category/CategoryBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace Budgetify.Entities.Tests.Category.Domain.Category;
+
+using System;
+
+using Budgetify.Entities.Category.Domain;
+
+internal class CategoryBuilder
+{
+    private readonly int _id = 1;
+    private readonly Guid _uid = Guid.NewGuid();
+    private readonly DateTime _createdOn = new(2024, 1, 17);
+    private DateTime? _deletedOn = null;
+    private readonly int _userId = 2;
+    private readonly string _name = "Name";
+    private readonly string _type = "EXPENSE";
+
+    internal Category Build()
+    {
+        return Category.Create(
+            id: _id,
+            uid: _uid,
+            createdOn: _createdOn,
+            deletedOn: _deletedOn,
+            userId: _userId,
+            name: _name,
+            type: _type).Value;
+    }
+
+    internal CategoryBuilder WithDeletedOn(DateTime deletedOn)
+    {
+        _deletedOn = deletedOn;
+        return this;
+    }
+}
---
+++ Tests/Api/Budgetify.Entities.Tests/Category/Domain/Category/Delete/DeleteShould.cs
@@ -0,0 +1,54 @@
+﻿namespace Budgetify.Entities.Tests.Category.Domain.Category;
+
+using System;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Category.Domain;
+using Budgetify.Entities.Common.Enumerations;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture(Category = nameof(DeleteShould))]
+public class DeleteShould
+{
+    [Test]
+    public void WhenAlreadyDeleted_WontOverrideDeletedOn()
+    {
+        // Arrange
+        DateTime deletedOn = new(2024, 1, 17);
+
+        Category category = new CategoryBuilder()
+            .WithDeletedOn(new DateTime(2024, 1, 15))
+            .Build();
+
+        // Act
+        Result result = category.Delete(deletedOn);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        category.State.Should().Be(EntityState.Unchanged);
+        category.DeletedOn.Should().Be(new DateTime(2024, 1, 15));
+    }
+
+    [Test]
+    public void WhenNotDeleted_WillSetDeletedOn()
+    {
+        // Arrange
+        DateTime deletedOn = new(2024, 1, 17);
+
+        Category category = new CategoryBuilder()
+            .Build();
+
+        // Act
+        Result result = category.Delete(deletedOn);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        category.State.Should().Be(EntityState.Modified);
+        category.DeletedOn.Should().Be(deletedOn);
+    }
+}
