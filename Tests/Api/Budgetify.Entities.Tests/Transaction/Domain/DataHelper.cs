8dfd0c3fe7358119581bb5218285d692b9ad5903
Viktor Stojkoski <viktor.98@hotmail.com>
2024-01-22 20:53:51 +0100

    create from storage tests

---
+++ Tests/Api/Budgetify.Entities.Tests/Transaction/Domain/DataHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Budgetify.Entities.Tests.Transaction.Domain;
+
+using System.Collections.Generic;
+
+using TransactionAttachmentEntity = Entities.Transaction.Domain.TransactionAttachment;
+
+internal class DataHelper
+{
+    internal static IEnumerable<TransactionAttachmentEntity> CreateTransactionAttachments(
+        int transactionId = 1)
+    {
+        return new List<TransactionAttachmentEntity>()
+        {
+            TransactionAttachmentEntity.Create(
+                createdOn: new(2024, 1, 20),
+                transactionId: transactionId,
+                filePath: "transactions/path-1",
+                name: "Test name 1").Value,
+
+            TransactionAttachmentEntity.Create(
+                createdOn: new(2024, 1, 21),
+                transactionId: transactionId,
+                filePath: "transactions/path-2",
+                name: "Test name 2").Value,
+
+            TransactionAttachmentEntity.Create(
+                createdOn: new(2024, 1, 22),
+                transactionId: transactionId,
+                filePath: "transactions/path-3",
+                name: "Test name 3").Value
+        };
+    }
+}
---
+++ Tests/Api/Budgetify.Entities.Tests/Transaction/Domain/Transaction/CreateFromStorage/CreateFromStorageShould.cs
@@ -0,0 +1,127 @@
+﻿namespace Budgetify.Entities.Tests.Transaction.Domain.Transaction;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Budgetify.Common.Results;
+using Budgetify.Entities.Common.Enumerations;
+using Budgetify.Entities.Transaction.Domain;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using static CommonTestsHelper;
+using static DataHelper;
+
+[TestFixture(Category = nameof(CreateFromStorageShould))]
+public class CreateFromStorageShould
+{
+    [Test]
+    public void WhenTypeInvalid_WillReturnErrorResult()
+    {
+        // Arrange
+        int id = RandomId();
+        Guid uid = Guid.NewGuid();
+        DateTime createdOn = new(2024, 1, 22);
+        int userId = RandomId();
+        int accountId = RandomId();
+        int fromAccountId = RandomId();
+        int categoryId = RandomId();
+        int currencyId = RandomId();
+        int merchantId = RandomId();
+        string type = "INVALID";
+        decimal amount = 10000;
+        DateTime date = new(2024, 1, 20);
+        string description = "Test description";
+        bool isVerified = true;
+        IEnumerable<TransactionAttachment> attachments = Enumerable.Empty<TransactionAttachment>();
+
+        // Act
+        Result<Transaction> result =
+            Transaction.Create(
+                id: id,
+                uid: uid,
+                createdOn: createdOn,
+                deletedOn: null,
+                userId: userId,
+                accountId: accountId,
+                fromAccountId: fromAccountId,
+                categoryId: categoryId,
+                currencyId: currencyId,
+                merchantId: merchantId,
+                type: type,
+                amount: amount,
+                date: date,
+                description: description,
+                isVerified: isVerified,
+                attachments: attachments);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Message.Should().Be(ResultCodes.TransactionTypeInvalid);
+    }
+
+    [Test]
+    public void WhenArgumentsCorrect_WillCreateTransactionFromStorage()
+    {
+        // Arrange
+        int id = RandomId();
+        Guid uid = Guid.NewGuid();
+        DateTime createdOn = new(2024, 1, 22);
+        int userId = RandomId();
+        int accountId = RandomId();
+        int fromAccountId = RandomId();
+        int categoryId = RandomId();
+        int currencyId = RandomId();
+        int merchantId = RandomId();
+        string type = "EXPENSE";
+        decimal amount = 10000;
+        DateTime date = new(2024, 1, 20);
+        string description = "Test description";
+        bool isVerified = true;
+        IEnumerable<TransactionAttachment> attachments = CreateTransactionAttachments(id);
+
+        // Act
+        Result<Transaction> result =
+            Transaction.Create(
+                id: id,
+                uid: uid,
+                createdOn: createdOn,
+                deletedOn: null,
+                userId: userId,
+                accountId: accountId,
+                fromAccountId: fromAccountId,
+                categoryId: categoryId,
+                currencyId: currencyId,
+                merchantId: merchantId,
+                type: type,
+                amount: amount,
+                date: date,
+                description: description,
+                isVerified: isVerified,
+                attachments: attachments);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        result.Value.State.Should().Be(EntityState.Unchanged);
+        result.Value.Id.Should().Be(id);
+        result.Value.Uid.Should().Be(uid);
+        result.Value.CreatedOn.Should().Be(createdOn);
+        result.Value.DeletedOn.Should().BeNull();
+        result.Value.UserId.Should().Be(userId);
+        result.Value.AccountId.Should().Be(accountId);
+        result.Value.FromAccountId.Should().Be(fromAccountId);
+        result.Value.CategoryId.Should().Be(categoryId);
+        result.Value.CurrencyId.Should().Be(currencyId);
+        result.Value.MerchantId.Should().Be(merchantId);
+        result.Value.Type.Name.Should().Be(type);
+        result.Value.Amount.Should().Be(amount);
+        result.Value.Date.Should().Be(date);
+        result.Value.Description.Should().Be(description);
+        result.Value.IsVerified.Should().Be(isVerified);
+        result.Value.Attachments.Should().BeEquivalentTo(attachments);
+    }
+}
