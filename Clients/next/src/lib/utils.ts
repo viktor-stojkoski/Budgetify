5c0d3bb78927c38565769628bbab1754249882af
Viktor Stojkoski <viktor.98@hotmail.com>
2025-06-02 00:17:14 +0200

    created locale switcher

--- Clients/next/src/lib/utils.ts
+++ Clients/next/src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getCookie(name: string): string | undefined {
+  return document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(`${name}=`))
+    ?.split("=")[1];
+}
+
+export function setCookie(name: string, value: string): void {
+  document.cookie = `${name}=${value};path=/`;
 }
